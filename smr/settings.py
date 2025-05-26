12b9c8038b53a3bf8ec942fff6f55c0b00eb055d
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-05-26 12:04:56 +0200

    chore(i18n): enable LocaleMiddleware, LANGUAGES, LOCALE_PATHS

--- smr/settings.py
+++ smr/settings.py
@@ -48,17 +48,35 @@ INSTALLED_APPS = [
     'base'
 ]
 
+LANGUAGE_CODE = "en"
+
+LANGUAGES = [
+    ("en", "English"),
+    ("fr", "Français"),
+    ("rw", "Kinyarwanda"),
+    ("sw", "Kiswahili"),
+]
+
+LOCALE_PATHS = [
+    BASE_DIR / "locale",
+]
+
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "django.middleware.locale.LocaleMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+USE_I18N = True
+USE_L10N = True
+USE_TZ  = True
 
 ROOT_URLCONF = 'smr.urls'
 
