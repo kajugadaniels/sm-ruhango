96b4da209e5723ad2477f2f87615ae944e33adb4
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-05-27 10:25:34 +0200

    Add views functions

--- base/views.py
+++ base/views.py
@@ -22,3 +22,27 @@ def change_language(request: HttpRequest, lang_code: str) -> HttpResponse:
 
 def home(request):
     return render(request, 'pages/index.html')
+
+def massSchedule(request):
+    return render(request, 'pages/mass-schedule.html')
+
+def homilies(request):
+    return render(request, 'pages/homilies.html')
+
+def healingPrayers(request):
+    return render(request, 'pages/healing-prayers.html')
+
+def events(request):
+    return render(request, 'pages/events/index.html')
+
+def eventDetails(request):
+    return render(request, 'pages/events/show.html')
+
+def testimonies(request):
+    return render(request, 'pages/testimonies.html')
+
+def gallery(request):
+    return render(request, 'pages/gallery.html')
+
+def donate(request):
+    return render(request, 'pages/donate.html')
