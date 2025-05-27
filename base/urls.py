761c13a66e4c2edb77e7492e5b302ea9230f73ad
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-05-27 10:23:13 +0200

    Add routes

--- base/urls.py
+++ base/urls.py
@@ -9,4 +9,11 @@ urlpatterns = [
     path('change-language/<str:lang_code>/', change_language, name='change_language'),
 
     path('', home, name='home'),
+    path('mass-schedule/', massSchedule, name='massSchedule'),
+    path('homilies/', homilies, name='homilies'),
+    path('healing-prayers/', healingPrayers, name='healingPrayers'),
+    path('events/', events, name='events'),
+    path('testimonies/', testimonies, name='testimonies'),
+    path('gallery/', gallery, name='gallery'),
+    path('donate/', donate, name='donate'),
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
